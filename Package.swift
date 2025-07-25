821bbbc9f7dc74c4f509d1484803cce3d7593b9e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-25 16:12:40 +0900

    feat: swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,12 @@
 // swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLab",
+    targets: [
+        .executableTarget(
+            name: "SwiftLab",
+        )
+    ]
+)
